ccd442ae72d85ef76b9a3299a3405803de77e1a1
Joseph <josembi@gmail.com>
2017-11-14 22:36:26 +0100

    Add Form

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -9,11 +9,17 @@ class DocsController < ApplicationController
 	end
 
 	def new
-		
+		@doc = Doc.new 
 	end
 
 	def create
-		
+		@doc = Doc.new(doc_params)
+
+		if @doc.save
+			redirect_to @doc
+		else
+			render 'new'
+		end
 	end
 
 	def edit
@@ -28,6 +34,7 @@ class DocsController < ApplicationController
 		
 	end
 
+
 	private
 
 	def find_doc
@@ -35,7 +42,7 @@ class DocsController < ApplicationController
 	end
 
 	def doc_params
-		
+		params.require(:doc).permit(:title, :content)
 	end
 
 end
